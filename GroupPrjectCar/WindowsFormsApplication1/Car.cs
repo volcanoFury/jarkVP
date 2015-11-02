e8834b79c76ee3a764763a1bbc9bbe39be4100fa
JakeBloor10 <jakebloor10@gmail.com>
2015-11-02 10:40:33 +0000

    Jake  Changes

---
+++ GroupPrjectCar/WindowsFormsApplication1/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Car
+    {
+        private int carID;
+
+        public int CarID
+        {
+            get { return carID; }
+            set { carID = value; }
+        }
+        private int year;
+        private int miles;
+        private string brand;
+        private string model;
+        private string bodyType;
+        private string gearBox;
+        private string inforamtion;
+        private DateTime carAdded; 
+        private DateTime carLasrEdited;
+    }
+}
---
+++ GroupPrjectCar/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void viewToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void abouToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Create By, \nB00654860 - Jake Bloor \nB00654973 Andrew Robinson");
+        }
+    }
+}
