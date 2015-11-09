1683bff81c38cc0976b1a3adde06abe3a95c2c54
JakeBloor10 <jakebloor10@gmail.com>
2015-11-09 14:35:40 +0000

    UpdateCodeadded

--- GroupPrjectCar/WindowsFormsApplication1/frmUpdateDelete.cs
+++ GroupPrjectCar/WindowsFormsApplication1/frmUpdateDelete.cs
@@ -13,6 +13,12 @@ namespace WindowsFormsApplication1
 {
     public partial class frmUpdateDelete : Form
     {
+        int year = 0;
+        double price = 0, miles = 0;
+        DateTime currentDate = DateTime.Today;
+        String brand, model, addInfo;
+        String filename = "", filepath;
+
         List<string> upreDatebase = new List<string>(); 
 
         public frmUpdateDelete(List<string> datebase)
@@ -23,7 +29,7 @@ namespace WindowsFormsApplication1
 
         private void btnBrowse_Click(object sender, EventArgs e)
         {
-            String filename = "", filepath;
+            
             OpenFileDialog open = new OpenFileDialog() { Filter = "Image Files(*.jpeg;*.bmp;*.png;*.jpg)|*.jpeg;*.bmp;*.png;*.jpg" };
             if (open.ShowDialog() == DialogResult.OK)
             {
@@ -41,13 +47,65 @@ namespace WindowsFormsApplication1
 
         private void addNewCarToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            frmAddCar add = new frmAddCar();
+            frmAddCar add = new frmAddCar(upreDatebase);
             add.Show();
         }
 
         private void frmUpdateDelete_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            lblMultidate.Text = "" + currentDate;
+
+            try
+            {
+                brand = txtBrand.Text;
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Brand needs and Filled in");
+            }
+
+            try
+            {
+                model = txtModel.Text;
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Model needs and Filled in");
+            }
+
+            try
+            {
+                year = Convert.ToInt32(txtYear.Text);
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Year needs to be a Number and Filled in");
+            }
+
+            try
+            {
+                price = Convert.ToInt32(txtPrice.Text);
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Price needs to be a Number and Filled in");
+            }
+
+            try
+            {
+                miles = Convert.ToInt32(txtMiles.Text);
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Miles needs to be a Number and Filled in");
+            }
+
+            MessageBox.Show("" + brand + model + year + price + miles);
         }
     }
 }
