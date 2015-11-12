cfd1dfc5931fcdcc4ce6cdd432a1cbe3c958100f
volcanoFury <andrew@arablemen.com>
2015-11-12 19:12:49 +0000

    commits
    commits

--- GroupPrjectCar/WindowsFormsApplication1/addCar.cs
+++ GroupPrjectCar/WindowsFormsApplication1/addCar.cs
@@ -14,19 +14,38 @@ namespace WindowsFormsApplication1
     public partial class frmAddCar : Form
     {
         int year = 0;
+        double price = 0, miles = 0;
+        DateTime currentDate = DateTime.Today;
+        String brand, model;
 
         public frmAddCar()
         {
             InitializeComponent();
+            lblMultidate.Text = "" + currentDate;
         }
-
-        //private void radioButtonChecked
-        //{
-
-        //}
 
         private void btnSave_Click(object sender, EventArgs e)
         {
+            lblMultidate.Text = "" + currentDate;
+
+            try
+            {
+                brand = txtBrand.Text;
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Brand needs and Filled in");
+            }
+
+            try
+            {
+                model = txtModel.Text;
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Model needs and Filled in");
+            }
+
             try
             {
                 year = Convert.ToInt32(txtYear.Text);
@@ -35,6 +54,26 @@ namespace WindowsFormsApplication1
             {
                 MessageBox.Show("Year needs to be a Number and Filled in");
             }
+
+            try
+            {
+                price = Convert.ToInt32(txtPrice.Text);
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Price needs to be a Number and Filled in");
+            }
+
+            try
+            {
+                miles = Convert.ToInt32(txtMiles.Text);
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Miles needs to be a Number and Filled in");
+            }
+
+            MessageBox.Show("" + brand + model + year + price + miles); 
         }
 
         private void btnBrowse_Click(object sender, EventArgs e)
